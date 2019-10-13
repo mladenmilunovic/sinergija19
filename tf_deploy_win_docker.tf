e837a3ed47356aaeeb836c4dd3122a2752d6aa82
Mladen Milunovic <mladenmi@microsoft.com>
2019-10-13 17:16:51 +0200

    Storage added

--- tf_deploy_win_docker.tf
+++ tf_deploy_win_docker.tf
@@ -100,6 +100,18 @@ resource "random_id" "randomId" {
     byte_length = 8
 }
 
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                = "diag${random_id.randomId.hex}"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    location            = "westeurope"
+    account_replication_type = "LRS"
+    account_tier = "Standard"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
 # Create virtual machine
 resource "azurerm_virtual_machine" "myterraformvm" {
     name                  = "Server2016Docker"
