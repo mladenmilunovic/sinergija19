13f79507fa30171eb9eaddae30030077bbd6c1c7
Mladen Milunovic <mladenmi@microsoft.com>
2019-10-14 00:17:15 +0200

    Deploy Win Servers added

--- tf_deploy_win_servers.tf
+++ tf_deploy_win_servers.tf
@@ -1,3 +1,17 @@
+
+variable subscription_id {}
+variable tenant_id {}
+variable client_id {}
+variable client_secret {}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    subscription_id = "4e07c97e-fda4-4cf1-89a4-6fd1e7e5faf6"
+    client_id       = "d555bf26-1db7-4c4f-bb4c-f76acd23a29f"
+    client_secret   = "eec507c7-17db-41aa-82e3-93987e525826"
+    tenant_id       = "46f9fc40-f452-48e0-9661-ca193655481f"
+}
+
 module mycompute {
     source = "Azure/compute/azurerm"
     resource_group_name = "myResourceGroup"
