12933147b9ad67aeacf1fdf06cfda99a64cdfac8
Mladen Milunovic <mladenmi@microsoft.com>
2019-10-14 01:17:18 +0200

    Deploy Win Servers added

--- tf_deploy_win_docker2.tf
+++ tf_deploy_win_docker2.tf
@@ -84,7 +84,7 @@ resource "azurerm_network_interface" "myterraformnic" {
         name                          = "myNicConfiguration-myVM${count.index}"
         subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.myPublicIP.id}"        #network_interface_ids = ["${element(azurerm_network_interface.test.*.id, count.index)}"]
+        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"        #network_interface_ids = ["${element(azurerm_network_interface.test.*.id, count.index)}"]
     }
 
     tags = {
