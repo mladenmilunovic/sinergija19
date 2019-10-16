9b76556827e968c774e2c6e1bfb5ee5e4d65d8b1
Mladen Milunovic <mladenmi@microsoft.com>
2019-10-16 11:21:16 +0200

    port 80 added

--- windows/single_docker_host/tf_deploy_win_docker.tf
+++ windows/single_docker_host/tf_deploy_win_docker.tf
@@ -66,18 +66,6 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         destination_address_prefix = "*"
     }
 
-    security_rule {
-        name                       = "Web"
-        priority                   = 1010
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
     tags = {
         environment = "Terraform Demo"
     }
