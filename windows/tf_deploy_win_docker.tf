687094574a2316c093260e47f851c1aa683adc0b
Mladen Milunovic <mladenmi@microsoft.com>
2019-10-16 10:19:08 +0200

    Windows and Linux

--- windows/tf_deploy_win_docker.tf
+++ windows/tf_deploy_win_docker.tf
@@ -16,25 +16,25 @@ resource "azurerm_resource_group" "myterraformgroup" {
     }
 }
 
-# # Create virtual network
-# resource "azurerm_virtual_network" "myterraformnetwork" {
-#     name                = "Sinergija19-vnet"
-#     address_space       = ["10.0.0.0/24"]
-#     location            = "westeurope"
-#     resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "Sinergija19-vnet"
+    address_space       = ["10.0.0.0/24"]
+    location            = "westeurope"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
 
-#     tags = {
-#         environment = "Terraform Demo"
-#     }
-# }
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
 
-# # Create subnet
-# resource "azurerm_subnet" "myterraformsubnet" {
-#     name                 = "default"
-#     resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
-#     virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
-#     address_prefix       = "10.0.0.0/24"
-# }
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "default"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.0.0/24"
+}
 
 # Create public IPs
 resource "azurerm_public_ip" "myterraformpublicip" {
